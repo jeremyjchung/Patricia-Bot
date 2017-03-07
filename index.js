ba32c8c124588c22953fd1f17430ffa6a7b48424
Jeremy Chung <jeremyjchung@gmail.com>
2017-03-06 22:37:50 -0500

    please work

--- index.js
+++ index.js
@@ -28,26 +28,17 @@ app.get('/webhook/', function (req, res) {
 
 const token = process.env.FB_PAGE_ACCESS_TOKEN
 
-app.post('/webhook', function (req, res) {
-    var data = req.body
-
-    // Make sure this is a page subscription
-    if (data.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
-        data.entry.forEach(function(entry) {
-            let sender = entry.sender.id
-
-            // Iterate over each messaging event
-            entry.messaging.forEach(function(event) {
-                if (event.message && event.message.text) {
-          			    let text = event.message.text
-                    sendTextMessage(sender, text)
-        			  }
-      		  })
-        })
-        res.sendStatus(200)
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
     }
+    res.sendStatus(200)
 })
 
 function sendTextMessage(sender, text) {
