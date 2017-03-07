88c2e1df0742f09381d65bd48bd34a1a69baf763
Jeremy Chung <jeremyjchung@gmail.com>
2017-03-06 23:23:16 -0500

    Echo test (#1)
    Add echo functionality

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('Hello world, I am Patricia!')
 })
 
 // for Facebook verification
@@ -25,6 +25,51 @@ app.get('/webhook/', function (req, res) {
     }
     res.send('Error, wrong token')
 })
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+
+app.post('/webhook', function (req, res) {
+  var data = req.body
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+    data.entry.forEach(function(entry) {
+
+      entry.messaging.forEach(function(event) {
+        let sender = event.sender.id;
+        
+        if (event.message && event.message.text) {
+          sendTextMessage(sender, event.message.text);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      })
+    })
+
+    res.sendStatus(200);
+  }
+})
+
+function sendTextMessage(sender, text) {
+  	let messageData = { text:text }
+
+  	request({
+    		url: 'https://graph.facebook.com/v2.6/me/messages',
+    		qs: {access_token:token},
+    		method: 'POST',
+    		json: {
+    			recipient: {id:sender},
+    			message: messageData,
+    		}
+  	}, function(error, response, body) {
+    		if (error) {
+    			console.log('Error sending messages: ', error)
+    		} else if (response.body.error) {
+    			console.log('Error: ', response.body.error)
+    		}
+  	})
+}
+
 
 // Spin up the server
 app.listen(app.get('port'), function() {
